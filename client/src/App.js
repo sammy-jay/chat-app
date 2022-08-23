8f258edb6c516e859916b3a59f50b6953600019e
Tolu Soneye <sammyjayden27@gmail.com>
2022-08-23 02:28:57 +0000

    added some styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,22 @@
-import { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import ChatPage from "./components/ChatPage";
 import socketIO from "socket.io-client";
-const socket = socketIO.connect("http://localhost:5000");
 
+const socket = socketIO.connect(
+  "https://5000-sammyjay-ideasapplatest-ua9wvdo4v0r.ws-eu62.gitpod.io:5000/"
+);
 function App() {
-  useEffect(() => {
-    socket.on("connection");
-  }, []);
-
   return (
-    <div>
-      <p>Hello World!</p>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home socket={socket} />}></Route>
+          <Route path="/chat" element={<ChatPage socket={socket} />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
+
 export default App;
